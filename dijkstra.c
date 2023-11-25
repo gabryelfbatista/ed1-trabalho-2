5394facbfe8b166cbca917f11b6e0f6cd234f3cf
gabryelfbatista <gabryelfbatista@gmail.com>
2023-11-25 10:30:09 -0300

    otimização do código e comentários

--- dijkstra.c
+++ dijkstra.c
@@ -29,6 +29,7 @@ Vector *visited_construct(int size)
  * Função criada para pegar os vizinhos de um determinado nó
  * que está sendo visitado, ela checa se esse vizinho já foi viitado
  * se não foi adiciona no vetor de vizinhos
+ * @param visited vetor de booleano
  * @param nodes vetor de nós que vieram do Problem
  * @param source valor do nó de origem
 */
@@ -88,7 +89,6 @@ Vector *run_dijkstra(Problem *problem)
     ProblemNode *node_path;
     ProblemNode *origin = problem_node_construct(0, 0, 0); //nó de origem
     int node_removed_cost = 0;
-    int node_value = 0;
     bool was_visited = true;
 
     heap_push(not_visited, origin, origin->origin_cost);
@@ -102,14 +102,13 @@ Vector *run_dijkstra(Problem *problem)
         {
             node_removed_cost = node_removed->origin_cost;
 
+            //encontra o nó do vetor de caminhos referente ao nó removido e atualiza suas informações
             node_path = vector_get(paths, node_removed->node_value);
             node_path->source_node = node_removed->source_node;
             node_path->path_cost = node_removed->path_cost;
             node_path->origin_cost = node_removed->origin_cost;
 
-            node_value = node_removed->node_value;   
-
-            Vector *neighbors = get_neighbors(visited, p->nodes, node_value);
+            Vector *neighbors = get_neighbors(visited, p->nodes, node_removed->node_value);
 
             // adiciona os vizinhos na heap somando o custo total pra origem
             for (int i = 0; i < vector_size(neighbors); i++)
@@ -122,7 +121,7 @@ Vector *run_dijkstra(Problem *problem)
             vector_destroy(neighbors);
 
             //marca como visitado depois que adiciona os vizinhos na heap
-            vector_set(visited, node_value, &was_visited); 
+            vector_set(visited, node_removed->node_value, &was_visited); 
         }               
     };
 
